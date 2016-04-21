db661ec65089a4f0a95ded24fe521189f4d77c5a
Kirin MORINO <info@o8o.jp>
2016-04-21 20:26:48 +0900

    :up: Updated Files & Fixed README.md

---
+++ hsp.rtm/hsp.d/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+
+namespace hsp.d
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
