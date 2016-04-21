80501cca208d2866ebf2ec7ad6be2ec828e0152d
Kirin MORINO <info@o8o.jp>
2016-04-21 17:28:12 +0900

    :up: Updated Setup Tools

---
+++ setup/install/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Security.Principal;
+
+namespace install
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!IsAdministrator)
+            {
+                var psi = new ProcessStartInfo
+                {
+                    UseShellExecute = true,
+                    FileName = Application.ExecutablePath,
+                    Verb = "runas",
+                    Arguments = string.Join(" ", args)
+                };
+
+                try
+                {
+                    Process.Start(psi);
+                }
+                catch (Win32Exception)
+                {
+                    return;
+                }
+            }
+            else
+            {
+                var isVisualStudioCodeInstalled =
+                            MessageBox.Show("Visual Studio Codeをインストールしていますか？", "Step 1", MessageBoxButtons.YesNo);
+                if (isVisualStudioCodeInstalled == DialogResult.No)
+                {
+                    MessageBox.Show("Visual Studio Codeをインストールして下さい");
+                    Process.Start("https://www.visualstudio.com/ja-jp/products/code-vs.aspx");
+                    return;
+                }
+
+                var agree = MessageBox.Show("hsp.rtmをインストールしますか？", "Step 2", MessageBoxButtons.OKCancel);
+                if (agree == DialogResult.OK)
+                {
+                    Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) +
+                                              "\\.vscode\\extensions\\hsp-rtm");
+                    CopyDirectory(Directory.GetCurrentDirectory() + "\\data",
+                        Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\.vscode\\extensions\\hsp-rtm");
+                    MessageBox.Show("インストールが完了しました!");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 管理者権限として起動しているか
+        /// </summary>
+        static bool IsAdministrator
+        {
+            get
+            {
+                var principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+
+        /// <summary>
+        /// ディレクトリをコピーする
+        /// </summary>
+        /// <param name="sourceDirName">コピーするディレクトリ</param>
+        /// <param name="destDirName">コピー先のディレクトリ</param>
+        static void CopyDirectory(string sourceDirName, string destDirName)
+        {
+            //コピー先のディレクトリがないときは作る
+            if (!System.IO.Directory.Exists(destDirName))
+            {
+                System.IO.Directory.CreateDirectory(destDirName);
+                //属性もコピー
+                System.IO.File.SetAttributes(destDirName,
+                    System.IO.File.GetAttributes(sourceDirName));
+            }
+
+            //コピー先のディレクトリ名の末尾に"\"をつける
+            if (destDirName[destDirName.Length - 1] !=
+                    System.IO.Path.DirectorySeparatorChar)
+                destDirName = destDirName + System.IO.Path.DirectorySeparatorChar;
+
+            //コピー元のディレクトリにあるファイルをコピー
+            string[] files = System.IO.Directory.GetFiles(sourceDirName);
+            foreach (string file in files)
+                System.IO.File.Copy(file,
+                    destDirName + System.IO.Path.GetFileName(file), true);
+
+            //コピー元のディレクトリにあるディレクトリについて、再帰的に呼び出す
+            string[] dirs = System.IO.Directory.GetDirectories(sourceDirName);
+            foreach (string dir in dirs)
+                CopyDirectory(dir, destDirName + System.IO.Path.GetFileName(dir));
+        }
+    }
+}
---
+++ setup/uninstall/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Security.Principal;
+
+namespace uninstall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!IsAdministrator)
+            {
+                var psi = new ProcessStartInfo
+                {
+                    UseShellExecute = true,
+                    FileName = Application.ExecutablePath,
+                    Verb = "runas",
+                    Arguments = string.Join(" ", args)
+                };
+
+                try
+                {
+                    Process.Start(psi);
+                }
+                catch (Win32Exception)
+                {
+                    return;
+                }
+            }
+            else
+            {
+                var agree = MessageBox.Show("hsp.rtmをアンインストールしますか？", "hsp.rtm", MessageBoxButtons.OKCancel);
+                if (agree == DialogResult.OK)
+                {
+                    var rtm = Process.GetProcessesByName("hsp.rtm");
+                    foreach (var p in rtm)
+                    {
+                        p.Kill();
+                    }
+                    var watcher = Process.GetProcessesByName("hsp.watcher");
+                    foreach (var p in watcher)
+                    {
+                        p.Kill();
+                    }
+
+                    Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\.vscode\\extensions\\hsp-rtm", true);
+                    MessageBox.Show("アンインストールが完了しました!");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 管理者権限として起動しているか
+        /// </summary>
+        static bool IsAdministrator
+        {
+            get
+            {
+                var principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+    }
+}
